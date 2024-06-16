272b4461586ca4ead86476e358454f3477e94ee3
ifetreaty <68975552+ifetreaty@users.noreply.github.com>
2024-06-16 20:13:47 +0100

    feat(theme): set up theme across project to include colors and font styles

---
+++ Lendsqr/src/theme/fonts.ts
@@ -0,0 +1,6 @@
+const fonts = {
+  heading: "Work Sans, sans-serif",
+  body: "Work Sans, sans-serif",
+};
+
+export default fonts;
---
+++ Lendsqr/src/theme/index.ts
@@ -0,0 +1,4 @@
+// index.ts
+export { default as colors } from "./colors";
+export { default as fonts } from "./fonts";
+export { default as styles } from "./styles";
