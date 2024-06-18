8a7bbd6c7a872deeda6d210b647d7591566b4109
ifetreaty <68975552+ifetreaty@users.noreply.github.com>
2024-06-18 13:50:08 +0100

    feat(user details): set up user context to manage state

--- Lendsqr/src/App.tsx
+++ Lendsqr/src/App.tsx
@@ -1,23 +1,28 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AppSidebar from "./layouts/AppLayout/AppSidebar";
-import UsersPage from "./pages/Users";
 import LandingNavbar from "./layouts/LandingLayout/LandingNavbar";
 import "./styles/main.scss";
+import UsersPage from "./pages/UsersPage";
+import { UserProvider } from "./context/UserContext";
+import UserDetailsPage from "./pages/UserDetailsPage";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <LandingNavbar />
-        <AppSidebar />
-        <main>
-          <Routes>
-            <Route path="/app/users" element={<UsersPage />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <UserProvider>
+      <Router>
+        <div className="App">
+          <LandingNavbar />
+          <AppSidebar />
+          <main>
+            <Routes>
+              <Route path="/app/users" element={<UsersPage />} />
+              <Route path="/user-details" element={<UserDetailsPage />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </UserProvider>
   );
 }
 
--- Lendsqr/src/modules/Users/UserTable/DropdownMenu/DropdownMenu.tsx
+++ Lendsqr/src/modules/Users/UserTable/DropdownMenu/DropdownMenu.tsx
@@ -1,15 +1,30 @@
+import { useNavigate } from "react-router-dom";
 import {
   ActivateUserIcon,
   DeleteFriendIcon,
   EyeOpenIcon,
 } from "../../../../components/icons/UserTableIcons";
+import { IUserData } from "../../../../types/userData";
 
 import "./DropdownMenu.scss";
+import { useUserContext } from "../../../../context/UserContext";
 
-export default function DropdownMenu() {
+interface IDropdownMenuProps {
+  user: IUserData;
+}
+
+export default function DropdownMenu({ user }: IDropdownMenuProps) {
+  const navigate = useNavigate();
+  const { setSelectedUser } = useUserContext();
+
+  const handleViewMore = () => {
+    setSelectedUser(user);
+    navigate("/user-details");
+  };
+
   return (
     <div className="dropdown-menu">
-      <div className="dropdown-item">
+      <div className="dropdown-item" onClick={handleViewMore}>
         <EyeOpenIcon />
         <span>View Details</span>
       </div>
